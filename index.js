d78ef833ae9e9064d67e214403a969ab7dbd4dc8
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-04-14 22:51:39 -0500

    Functions in JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+function calculateTax (amount) {
+    return amount * 0.01
+}
+
+function convertToUpperCase (text) {
+    return string.toUpperCase()
+}
+
+function findMaximum (num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+function isPalindrome(word) {
+    return word === word.split('').reverse().join('')
+}
+
+function calculateDiscountedPrice(originalPrice, discountedPercentage) {
+    const discountAmount = originalPrice * (discountPercentage/100)
+    return originalPrice - discountAmount
+}
+const originalPrice = 100
+const discountPercentage = 20
+const cost = calculateDiscountedPrice(100,20)
+console.log(cost)
 
 
 
